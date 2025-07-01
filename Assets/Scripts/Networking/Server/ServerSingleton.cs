6321e60636383c48aedb4e63987f9a9d4141b7ca
Bloodheaven <just.rohrbach@gmail.com>
2025-07-01 15:55:55 +0200

    Dedicated Server
    Dedicated Servers
    UGS Wrapper
    Server Build Prep
    
    Removal of Matchmaker & Multiplay because of Multiplayer Services

---
+++ Assets/Scripts/Networking/Server/ServerSingleton.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using UnityEngine;
+using Unity.Netcode;
+
+public class ServerSingleton : MonoBehaviour
+{
+
+    static ServerSingleton instance;
+    public ServerGameManager GameManager { get; private set; }
+
+    public static ServerSingleton Instance
+    {
+        get
+        {
+            if (instance != null) return instance;
+
+            instance = FindFirstObjectByType<ServerSingleton>();
+
+            if (instance == null)
+            {
+                Debug.LogError("ServerSingleton not in Scene!");
+                return null;
+            }
+
+            return instance;
+        }
+
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public async Task CreateServer()
+    {
+        await UnityServices.InitializeAsync();
+
+        GameManager = new ServerGameManager
+        (
+            ApplicationData.IP(),
+            ApplicationData.Port(),
+            ApplicationData.QPort(),
+            NetworkManager.Singleton
+        );
+    }
+
+    private void OnDestroy()
+    {
+        GameManager?.Dispose();
+    }
+
+
+}
